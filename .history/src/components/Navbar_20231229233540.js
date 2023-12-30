008170e9fe135de826f9fb310c73f97c6bedc58b
James Song <james.song017@gmail.com>
2023-12-29 23:42:11 -0500

    updated images

--- .history/src/components/Navbar_20231229233540.js
+++ .history/src/components/Navbar_20231229233540.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Navbar() {
     return (
         <nav className="navbar">
-            <img src='/my-city-checklist/images/logo.png' alt="logo"/>
+            <img src='./my-city-checklist/images/logo.png' alt="logo"/>
             <p>my city checklist.</p>
         </nav>
     )
